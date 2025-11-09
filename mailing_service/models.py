d92fdcc98ce83c399e79b3b84c45fe5599bf510f
Hristyaa <shhs200799@gmail.com>
2025-11-09 21:49:46 +0900

    model Mailing

--- mailing_service/models.py
+++ mailing_service/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class MailingRecipient(models.Model):
@@ -30,4 +31,33 @@ class MailingMessage(models.Model):
         verbose_name_plural = "Сообщения"
         ordering = [
             "subject",
+        ]
+
+
+class Mailing(models.Model):
+    CREATED = 'created'
+    LAUNCHED = 'launched'
+    COMPLETED = 'completed'
+
+    MAILING_STATUS = [
+        (CREATED, 'Создана'),
+        (LAUNCHED, 'Запущена'),
+        (COMPLETED, 'Завершена'),
+    ]
+    launched_at = models.DateTimeField(null=True, blank=True, verbose_name='Дата и время первой отправки')
+    completed_at = models.DateTimeField(null=True, blank=True, verbose_name='Дата и время окончания отправки')
+    status = models.CharField(max_length=12, choices=MAILING_STATUS, default=CREATED, verbose_name='Статус рассылки')
+    message = models.ForeignKey(MailingMessage, on_delete=models.CASCADE, null=True, blank=True,
+                                verbose_name='Сообщение')
+    recipients = models.ManyToManyField(MailingRecipient, blank=True, verbose_name='Получатели')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания рассылки')
+
+    def __str__(self):
+        return f'Рассылка создана {self.created_at}, статус: {self.status}'
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
+        ordering = [
+            'status',
         ]
