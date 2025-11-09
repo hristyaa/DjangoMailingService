ff342fe1fbec7ba18d9ff71758ae1f58035ab176
Hristyaa <shhs200799@gmail.com>
2025-11-09 17:12:27 +0900

    MailingRecipientDetailView form

--- mailing_service/urls.py
+++ mailing_service/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.urls import include, path
 
 from mailing_service.apps import MailingServiceConfig
-from mailing_service.views import HomePageView, MailingRecipientListView, MailingRecipientCreateView
+from mailing_service.views import *
 
 # from . import views
 
@@ -14,6 +14,8 @@ urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("clients/", MailingRecipientListView.as_view(), name="clients"),
     path("clients/create/", MailingRecipientCreateView.as_view(), name="create_client"),
+    path("clients/<int:pk>/", MailingRecipientDetailView.as_view(), name="detail_client"),
+    path("clients/update/", MailingRecipientUpdateView.as_view(), name="update_client"),
     # path("posts/<int:pk>/", BlogPostDetailView.as_view(), name="post_detail"),
     # path("posts/create/", BlogPostCreateView.as_view(), name="post_create"),
     # path("posts/update/<int:pk>/", BlogPostUpdateView.as_view(), name="post_update"),
--- mailing_service/views.py
+++ mailing_service/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -24,3 +24,20 @@ class MailingRecipientCreateView(CreateView):
     context_object_name = 'client'
     template_name = 'mailing_service/mailing_recipient_form.html'
     success_url = reverse_lazy('mailing_service:clients')
+
+
+class MailingRecipientDetailView(DetailView):
+    model = MailingRecipient
+    context_object_name = 'client'
+    template_name = 'mailing_service/mailing_recipient_detail.html'
+
+
+class MailingRecipientUpdateView(UpdateView):
+    model = MailingRecipient
+    fields = ['name', 'email', 'comment']
+    context_object_name = 'client'
+    template_name = 'mailing_service/mailing_recipient_form.html'
+    success_url = reverse_lazy('mailing_service:clients')
+
+    # def get_success_url(self):
+    #     return reverse('mailing_service:client_detail', args=[self.kwargs.get('pk')])
