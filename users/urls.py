48a0ce9eedd861145f902fd5f761e94244f08a23
Hristyaa <shhs200799@gmail.com>
2025-12-07 15:17:24 +0900

    login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
@@ -13,7 +13,8 @@ from users.apps import UsersConfig
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name='login.html')),
+    path("login/", LoginView.as_view(template_name='login.html'), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
 
 if settings.DEBUG:
