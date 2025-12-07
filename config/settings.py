4e7b0c81d46b3a3cf097284f16e8582ee6b1cb24
Hristyaa <shhs200799@gmail.com>
2025-12-07 14:08:26 +0900

    model User (app users), migrate

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'mailing_service',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,8 @@ STATICFILES_DIRS = (BASE_DIR / 'static', )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
